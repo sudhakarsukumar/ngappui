146d76e47d341d87e862562bf93f18f926d5890a
sudhakarsukumar <sudhakar.sukumar92@gmail.com>
2019-11-04 09:20:20 -0800

    dashbaord

--- src/assets/js/controllers.js
+++ src/assets/js/controllers.js
@@ -1,1 +1,34 @@
 // controller.js
+angular
+    .module('app')
+    .controller('cardChartCtrl1', cardChartCtrl1)
+
+    cardChartCtrl1.$inject = ['$scope'];
+function cardChartCtrl1($scope) {
+    $scope.filters = [{
+            'filterId': 1,
+            'time': 'last 24 hours',
+        },
+        {
+            'filterId': 2,
+            'time': 'all',
+        },
+        {
+            'filterId': 3,
+            'time': 'last hour',
+        },
+        {
+            'filterId': 4,
+            'time': 'today',
+        },
+        {
+            'filterId': 5,
+            'time': 'yersteday',
+        }
+    ];
+    $scope.selected = 0;
+
+    $scope.select = function(index) {
+        $scope.selected = index;
+    };
+}
